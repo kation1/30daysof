a5bc21a02df2f9f5244e30ccc8704f9501484da2
Kation <Klpidek@gmail.com>
2018-10-11 20:11:29 -0400

    Days 5 & 6

---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+	static void Main(String[] args)
+	{
+		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+		int L = Convert.ToInt32(Console.ReadLine());
+		string[] words = new string[L];
+		int i = 0;
+
+		while (i < L)
+		{
+			string w = Console.ReadLine();
+			words[i++] = w;
+		}
+
+		i = 0;
+		while (i < L)
+		{
+			char[] c = words[i].ToCharArray();
+			string evens = "";
+			string odds = "";
+			for (int x = 0; x < c.Length; x++)
+			{
+				if (x % 2 == 0) evens += c[x];
+				if (x % 2 != 0) odds += c[x];
+			}
+			Console.WriteLine(evens + " " + odds);
+			i++;
+
+		}
+	}
+}
